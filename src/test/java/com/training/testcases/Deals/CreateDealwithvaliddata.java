8c1d4aa47814fbb80518b6fa1d5272a38ca1ef56
VENKATA BHARADWAJ ATTULURI <venkatabharadwajvb8@gmail.com>
2023-10-13 11:07:09 +0530

    Added functionalities

--- src/test/java/com/training/testcases/Deals/CreateDealwithvaliddata.java
+++ src/test/java/com/training/testcases/Deals/CreateDealwithvaliddata.java
@@ -1,4 +1,5 @@
 package com.training.testcases.Deals;
+
 import com.training.pages.BasePage;
 import com.training.pages.DealsPage;
 
@@ -25,16 +26,16 @@ public class CreateDealwithvaliddata extends BaseTest {
         // loginPage.checkPageHeader("Deals");
 
         DealsPage dealsPage = new DealsPage(getDriver());
-       // dealsPage.clickonCreate();
+        // dealsPage.clickonCreate();
         ExtentTestManager.getTest().pass("Create a Deal");
         HashMap<String, String> deals = new HashMap<String, String>();
-      //String s = CommonUtil.getRandomString("Training", 14);
-        deals.put("Title","NewDeal" );
+        //String s = CommonUtil.getRandomString("Training", 14);
+        deals.put("Title", "NewDeal");
         deals.put("Assigned To", "werwer werwer");
         deals.put("Company", "QualiZeal");
         deals.put("Contacts", "Bharat Bharath");
         deals.put("Close Date", "9/28/2023");
-       deals.put("Tags","Its service");
+        deals.put("Tags", "Its service");
         deals.put("Description", "Its sample deal case");
         deals.put("Probability", "80%");
         deals.put("Amount", "20000");
@@ -43,25 +44,6 @@ public class CreateDealwithvaliddata extends BaseTest {
         deals.put("Status", "Active");
         dealsPage.createDeals(deals);
 
-        Thread.sleep(5000);
-        dealsPage.clickOnSaveButton();
-//        dealsPage.refreshingPage();
-//        dealsPage.clickOnSaveButton();
-      //  ExtentTestManager.getTest().pass("Created a deal successfully");
-
-
-//        dealsPage.saveDetails();
-//        // dealsPage.homePage();
-//       dealsPage.verify();
-////        dealsPage.deleteRecord(deals);
-//   // dealsPage.deletearecord();
 
     }
-//    public String randomStringGenerator(int num){
-//        return RandomStringUtils.randomAlphabetic(num);
-//    }
-//    public String randomIntGenerator(int num){
-//        return RandomStringUtils.randomAlphanumeric(num);
-//    }
-
 }
